c00372e9ab17c43ef4867e47deb571db9b9667c8
Dmitrii Snitkin <snitkin.d@list.ru>
2020-09-29 11:12:27 +0300

    feat: implement min max avr functions with error if no params

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array.length === 0 || array === undefined) {
+      return 0;
+  }
+  let min = array[0];
+  for(let i = 0; i < array.length; i++) {
+    if (min > array[i + 1])
+        min = array[i + 1];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array.length === 0 || array === undefined) {
+        return 0;
+    }
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (max < array[i + 1])
+          max = array[i + 1];
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array.length === 0 || array === undefined) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
 }
